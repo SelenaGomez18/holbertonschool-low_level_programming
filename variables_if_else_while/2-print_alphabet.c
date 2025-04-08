f2462ad961a13e4f0f2f7d6e2f6f8b52bf843733
SelenaGomez18 <selenagomezrueda045@gmail.com>
2025-04-08 07:33:19 +0000

    Add a program that prints the alphabet in lowercase, followed by a new line.

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Description: This function prints all the lowercase letters of the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    char letra; 
+char letra;
 
-     for (letra ='a'; letra <= 'z'; letra++)
-
-     {
-        putchar(letra);
-     }
-
-     putchar('\n');
-return 0;
+/* Imprimir las letras del abecedario en minúsculas */
+for (letra = 'a'; letra <= 'z'; letra++)
+{
+putchar(letra);  /* Imprime cada letra */
+}
+putchar('\n');  /* Añadir un salto de línea al final */
+return (0);
 }
