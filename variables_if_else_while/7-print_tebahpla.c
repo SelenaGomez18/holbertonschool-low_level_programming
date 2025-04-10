574fbae5d0b467dcb53d7df82aa99a7d62b95f1f
SelenaGomez18 <selenagomezrueda045@gmail.com>
2025-04-10 06:20:53 +0000

    Add a program thats print alphabet in reverse following a new line

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Description: This function prints all alphabet the lowercase in reverse
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 
 {
-    char character;
+	char character;
 
-    for (character = 'z'; character >= 'a'; character--)
+	for (character = 'z'; character >= 'a'; character--)
 
-    {
-        putchar(character);
-    }
-    putchar(10);
-    return(0);
+	{
+		putchar(character);
+	}
+	putchar(10);
+	return (0);
 }
