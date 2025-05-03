750da8c599fd250cd5af76f6a98c2c867184bef0
SelenaGomez18 <selenagomezrueda045@gmail.com>
2025-05-03 01:20:30 -0500

    Add a function to copies string

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,8 +1,27 @@
 #include "main.h"
 
+/**
+ * _strncpy - a function copies string
+ *
+ * @dest: strin detination
+ * @src: string to copy
+ * @n: number of times to copy the characters
+ *
+ * Return: destination
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
-    int a = 0;
-    
-    for ( )
+	int a = 0;
+
+	for (; a < n && src[a] != '\0'; a++)
+	{
+		dest[a] = src[a];
+	}
+
+	for (; a < n; a++)
+	{
+		dest[a] = '\0';
+	}
+
+	return (dest);
 }
