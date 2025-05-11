c14f29e9af02549cd5984c9fa81790cc4e7577bf
SelenaGomez18 <selenagomezrueda045@gmail.com>
2025-05-11 18:18:12 -0500

    Add function that locates a substring

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -32,7 +32,8 @@
  char *_memcpy(char *, char *, unsigned int);
  char *_strchr(char *, char);
  unsigned int _strspn(char *, char *);
- char *_strpbrk(char *s, char *accept);
+ char *_strpbrk(char *, char *);
+ char *_strstr(char *, char *);
  void print_chessboard(char (*a)[8]);
 
  #endif /*_MAIN_H_ */
