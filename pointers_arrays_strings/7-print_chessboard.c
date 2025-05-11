d8b50f1d41cd11d1456f58ff7993503dd42bd951
SelenaGomez18 <selenagomezrueda045@gmail.com>
2025-05-11 16:23:59 -0500

    Add a function prints an 8x8 chessboard

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,18 +1,22 @@
 #include "main.h"
 
 /**
- *  
+ * print_chessboard - Prints the chessboard.
+ * @a: Pointer to a 2D array representing the chessboard (8x8).
+ *
+ * Description: This function prints an 8x8 chessboard, where each element
+ * of the array represents a piece or empty square.
  */
 void print_chessboard(char (*a)[8])
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i < 8; i++)
-    {
-        for (j = 0; j < 8; j++)
-        {
-            _putchar(a[i][j]);
-        }
-        _putchar('\n');
-    }
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
 }
