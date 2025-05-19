209389e4c7e0c891ba6fe945b05ef6e683e5cd58
SelenaGomez18 <selenagomezrueda045@gmail.com>
2025-05-18 23:13:12 -0500

    Add a function that returns the natural square root of a number. in 5. And Add a function that returns 1 if the input integer is a prime number, otherwise return 0. in 6

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,6 +1,37 @@
 #include "main.h"
 
+
+/* Forward declaration of the helper function */
+int sqrt_helper(int n, int i);
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: the number to calculate the square root of.
+ * Return: the natural square root, or -1 if it doesn't exist.
+ */
 int _sqrt_recursion(int n)
 {
-    
+	if (n < 0)
+		return (-1);
+	return (sqrt_helper(n, 0));
+}
+
+/**
+ * sqrt_helper - recursively tries to find the square root.
+ * @n: the original number.
+ * @i: the current number being tested as a possible square root.
+ * Return: the square root if found, or -1 if not.
+ */
+int sqrt_helper(int n, int i)
+
+{
+	if (i * i == n)
+	{
+		return (i);
+	}
+	if (i > n)
+	{
+		return (-1);
+	}
+	return (sqrt_helper(n, i + 1));
 }
--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,3 +1,35 @@
 #include "main.h"
 
+/* Helper function declaration */
+int check_prime(int n, int i);
+
+/**
+ * is_prime_number - returns 1 if n is a prime number, otherwise 0.
+ * @n: the number to check
+ * Return: 1 if prime, 0 if not
+ */
 int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (check_prime(n, 2));
+}
+
+/**
+ * check_prime - recursively checks if a number is divisible
+ * @n: the number to check
+ * @i: the current divisor being tested
+ * Return: 1 if prime, 0 otherwise
+ */
+int check_prime(int n, int i)
+{
+	if (i * i > n)
+	{
+		return (1);
+	}
+	if (n % i == 0)
+	{
+		return (0);
+	}
+	return (check_prime(n, i + 1));
+}
