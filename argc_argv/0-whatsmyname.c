f3362622787c1e048b6b19ee940a87bba4ad93c4
SelenaGomez18 <selenagomezrueda045@gmail.com>
2025-05-20 19:44:36 -0500

    Add a program that prints its name, followed by a new line.

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * 
+ */
+int main(int argc, int **argc)
+{
+    (void) argc;
+
+    printf("%s\n", argv[0]);
+    return (0);
+}
---
+++ argc_argv/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+int _putchar(char c);
+int main(int argc, char **argv);
+
+#endif /* MAIN_H */
