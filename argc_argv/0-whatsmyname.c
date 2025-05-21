e90cf23bdf114d76346db7df2d7ca4051af76fc9
SelenaGomez18 <selenagomezrueda045@gmail.com>
2025-05-20 19:52:53 -0500

    Add a program that prints its name, followed by a new line.

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 
+
 /**
- * 
+ * main - Entry point of the program
+ * @argc: Argument count (unused)
+ * @argv: Argument vector (array of strings)
+ *
+ * Description: Prints the name of the program followed by a new line.
+ * The name includes the path used to execute the program.
+ *
+ * Return: 0 on success
  */
 int main(int argc, char *argv[])
 {
-    (void) argc;
+	(void) argc;
 
-    printf("%s\n", argv[0]);
-    return (0);
+	printf("%s\n", argv[0]);
+	return (0);
 }
