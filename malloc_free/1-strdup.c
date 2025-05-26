8dd0d3e63ef1c862d8350f08cbcf07208675d164
SelenaGomez18 <selenagomezrueda045@gmail.com>
2025-05-25 20:54:23 -0500

    Add a function duplicates a string in a newly allocated memory space

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,23 +1,30 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ * _strdup - Duplicates a string in a newly allocated memory space.
+ * @str: The string to duplicate.
+ *
+ * Return: Pointer to the duplicated string, or NULL if str is NULL
+ * or if memory allocation fails.
+ */
 char *_strdup(char *str)
 {
-    char *copy = NULL;
-    int i, len = 0;
-    
-    if (str == NULL)
-        return (NULL);
+	char *copy = NULL;
+	int i, len = 0;
 
-    for (i = 0; str[i] != '\0'; i++)
-        len++;
+	if (str == NULL)
+		return (NULL);
 
-    copy = malloc((len + 1)*sizeof(char*));
-    if (copy == NULL)
-        return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+		len++;
 
-    for (i = 0; i <= len; i++)
-        copy[i] = str[i];
+	copy = malloc((len + 1) * sizeof(char *));
+	if (copy == NULL)
+		return (NULL);
 
-    return (copy);
+	for (i = 0; i <= len; i++)
+		copy[i] = str[i];
+
+	return (copy);
 }
