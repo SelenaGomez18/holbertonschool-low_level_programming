b17b8bd946c05f021ced0cb3387f5b0a9efda830
SelenaGomez18 <selenagomezrueda045@gmail.com>
2025-05-29 23:36:35 -0500

    Add a funtion that prints a struct dog

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,12 +1,22 @@
 #include <stdlib.h>
 #include "dog.h"
 
+/**
+ * init_dog - Initializes a variable of type struct dog
+ * @d: Pointer to the struct dog to initialize
+ * @name: Name to assign to the dog
+ * @age: Age to assign to the dog
+ * @owner: Owner to assign to the dog
+ *
+ * Description: This function assigns the given parameters to the
+ * corresponding members of the struct dog pointed to by d.
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-    if (d == NULL)
-        return;
-    
-    d->name = name;
-    d->age = age;
-    d->owner = owner;
+	if (d == NULL)
+		return;
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
