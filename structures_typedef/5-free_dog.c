4bad53ba8b5205bde303fb7ac97de60f25840cbd
SelenaGomez18 <selenagomezrueda045@gmail.com>
2025-06-01 19:08:00 -0500

    Add a function  that frees dogs

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -1,12 +1,16 @@
 #include "dog.h"
 #include <stdlib.h>
 
+/**
+ * free_dog - Frees a dog_t structure
+ * @d: Pointer to the dog_t structure to be freed
+ */
 void free_dog(dog_t *d)
 {
-    if (d != NULL)
-    {
-        free(d->name);
-        free(d->owner);
-        free(d);
-    }
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
 }
