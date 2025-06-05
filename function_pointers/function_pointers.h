3b60c97644e6a948546a3d0c710b4ddde8aae591
SelenaGomez18 <selenagomezrueda045@gmail.com>
2025-06-04 20:21:31 -0500

    Add a function that given as a parameter

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,11 +1,14 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
+#include <stddef.h>
 
 /*
  * Structs, enums and unions definitions
  * Typedefs
  * Function prototypes
  */
-void print_name(char *, void (*f)(char *));
+
+void print_name(char *, void (*)(char *));
+void array_iterator(int *, size_t, void (*)(int));
 
 #endif /*_MAIN_H_ */
