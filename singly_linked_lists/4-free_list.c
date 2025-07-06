9036173611528adffaeaed38d79edef2f4ad6296
SelenaGomez18 <selenagomezrueda045@gmail.com>
2025-07-05 20:44:50 -0500

    Add a function that frees a list_t list.

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,11 +1,14 @@
 #include "lists.h"
 #include <stdlib.h>
-
+/**
+ * free_list - Frees a list_t list
+ * @head: Pointer to the head of the list
+ */
 void free_list(list_t *head)
 {
-    for (; head != NULL; head=head->next)
-    {
-        free(head->str);
-        free(head);
-    }
+	for (; head != NULL; head=head->next)
+	{
+		free(head->str);
+		free(head);
+	}
 }
