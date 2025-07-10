5a9e2b815899049a739da5e3868584ba91fc3951
SelenaGomez18 <selenagomezrueda045@gmail.com>
2025-07-09 20:10:25 -0500

    Add a function that sets the value of a bit to 0 at a given index.

---
+++ bit_manipulation/3-set_bit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+    if (index >= sizeof(n) * 8)
+		return (-1);
+    
+    *n = *n | (1UL << index);
+    return (1);
+}
---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+        if (index >= sizeof(n) * 8)
+		return (-1);
+    
+    *n = *n & ~(1UL << index);
+    return (1);
+}
