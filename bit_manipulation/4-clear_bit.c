d6614a7eed2807591395592d9258b35607d210cd
SelenaGomez18 <selenagomezrueda045@gmail.com>
2025-07-09 22:58:15 -0500

    Add a function get endianness

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -1,11 +1,17 @@
 #include "main.h"
 
-
+/**
+ * clear_bit - Sets the value of a bit to 0 at a given index
+ * @n: Pointer to the number whose bit is to be cleared
+ * @index: Index of the bit to clear (starting from 0)
+ *
+ * Return: 1 if successful, -1 if an error occurred (e.g., index out of range)
+ */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-        if (index >= sizeof(n) * 8)
+	if (index >= sizeof(n) * 8)
 		return (-1);
-    
-    *n = *n & ~(1UL << index);
-    return (1);
+
+	*n = *n & ~(1UL << index);
+		return (1);
 }
