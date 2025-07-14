1f0311e3c87d4e2f8ab55de9b3b5a1aa9eeaf077
SelenaGomez18 <selenagomezrueda045@gmail.com>
2025-07-13 20:22:57 -0500

    Add a function that appends text at the end of a file.

--- file_io/main.h
+++ file_io/main.h
@@ -2,9 +2,14 @@
 #define _MAIN_H_
 
 #include <sys/types.h>
-
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 
 #endif /*_MAIN_H_ */
