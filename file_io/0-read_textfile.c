31f2ac9349e0c0efcaeac24e992dbbe26054e932
SelenaGomez18 <selenagomezrueda045@gmail.com>
2025-07-13 19:54:05 -0500

    Add  a function that creates a file.

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -3,31 +3,39 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+/**
+ * read_textfile - Reads a text file and prints it to POSIX stdout.
+ * @filename: Name of the file to read.
+ * @letters: Number of letters to read and print.
+ *
+ * Return: Actual number of letters read and printed.
+ * 0 on failure or if filename is NULL.
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    char *buffer = 0;
-    ssize_t r, w;
+	int fd;
+	char *buffer = 0;
+	ssize_t r, w;
 
-    if (!filename)
-        return (0);
-    
-    fd = open(filename, O_RDONLY);
-    if (fd < 0)
-        return (0);
-    
-    buffer = malloc(letters);
-    if (!buffer)
-    {
-        close(fd);
-        return (0);
-    }
+	if (!filename)
+		return (0);
 
-    r = read(fd, buffer, letters);
-    w = write(STDOUT_FILENO, buffer, r);
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return (0);
 
-    free(buffer);
-    close(fd);
+	buffer = malloc(letters);
+	if (!buffer)
+	{
+		close(fd);
+		return (0);
+	}
 
-    return ((r < 0 || w < 0 || w != r) ? 0 : w);
+	r = read(fd, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	free(buffer);
+	close(fd);
+
+	return ((r < 0 || w < 0 || w != r) ? 0 : w);
 }
