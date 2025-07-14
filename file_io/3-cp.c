6ce5ec6a3dd50d3be5cfcedbc61001c744d9c8f5
SelenaGomez18 <selenagomezrueda045@gmail.com>
2025-07-13 21:27:38 -0500

    Add a funcruin that copy a file another file

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,138 +1,89 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 #include <fcntl.h>
 #include <unistd.h>
-#include <sys/stat.h>
-#include <string.h>
+#include <stdlib.h>
 
-#define BUF_SIZE 1024
+#define BUFFER_SIZE 1024
 
 /**
- * print_error_and_exit - prints error message and exits program
- * @code: exit code to use
- * @msg: format string for error message
- * @arg: argument for format string (filename or fd casted to void*)
+ * print_error - Prints an error message to stderr and exits
+ * @code: Exit code
+ * @msg: Message to display
+ * @file: File name or descriptor value to include
  */
-void print_error_and_exit(int code, char *msg, void *arg)
+void print_error(int code, const char *msg, const char *file)
 {
-	if (msg && arg)
+	int i = 0;
+
+	while (msg[i])
+		write(STDERR_FILENO, &msg[i++], 1);
+	if (file)
 	{
-		if (code == 100)
-			dprintf(STDERR_FILENO, msg, (int)(long)arg);
-		else
-			dprintf(STDERR_FILENO, msg, (char *)arg);
+		i = 0;
+		while (file[i])
+			write(STDERR_FILENO, &file[i++], 1);
 	}
+	write(STDERR_FILENO, "\n", 1);
 	exit(code);
 }
 
 /**
- * open_files - opens source and destination files
- * @file_from: path to source file
- * @file_to: path to destination file
- * @fd_from: pointer to store source file descriptor
- * @fd_to: pointer to store destination file descriptor
- *
- * Opens file_from for reading and file_to for writing (create/truncate).
- * Exits with error if any open fails.
+ * copy_file - Copies the content of a file to another
+ * @file_from: Source file name
+ * @file_to: Destination file name
  */
-void open_files(char *file_from, char *file_to, int *fd_from, int *fd_to)
+void copy_file(const char *file_from, const char *file_to)
 {
-	mode_t old_mask = umask(0);
+	int fd_from, fd_to, r_bytes, w_bytes;
+	char buffer[BUFFER_SIZE];
 
-	*fd_from = open(file_from, O_RDONLY);
-	if (*fd_from == -1)
-		print_error_and_exit(98, "Error: Can't read from file %s\n", file_from);
+	fd_from = open(file_from, O_RDONLY);
+	if (fd_from == -1)
+		print_error(98, "Error: Can't read from file ", file_from);
 
-	*fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	umask(old_mask);
-
-	if (*fd_to == -1)
+	fd_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd_to == -1)
 	{
-		close(*fd_from);
-		print_error_and_exit(99, "Error: Can't write to %s\n", file_to);
+		close(fd_from);
+		print_error(99, "Error: Can't write to ", file_to);
 	}
-}
 
-/**
- * copy_data - copies data from one file descriptor to another
- * @fd_from: source file descriptor
- * @fd_to: destination file descriptor
- * @file_to: destination filename for error messages
- *
- * Reads from fd_from and writes to fd_to until done.
- * Exits with error on read or write failure.
- */
-void copy_data(int fd_from, int fd_to, char *file_to)
-{
-	ssize_t r, w;
-	char buf[BUF_SIZE];
-
-	while ((r = read(fd_from, buf, BUF_SIZE)) > 0)
+	while ((r_bytes = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
-		w = write(fd_to, buf, r);
-		if (w != r)
+		w_bytes = write(fd_to, buffer, r_bytes);
+		if (w_bytes != r_bytes)
 		{
 			close(fd_from);
 			close(fd_to);
-			print_error_and_exit(99, "Error: Can't write to %s\n", file_to);
+			print_error(99, "Error: Can't write to ", file_to);
 		}
 	}
-
-	if (r == -1)
+	if (r_bytes == -1)
 	{
 		close(fd_from);
 		close(fd_to);
-		print_error_and_exit(98, "Error: Can't read from file %s\n", file_to);
+		print_error(98, "Error: Can't read from file ", file_from);
 	}
-}
 
-/**
- * close_fds - closes two file descriptors and exits on failure
- * @fd_from: source file descriptor
- * @fd_to: destination file descriptor
- *
- * Closes both descriptors; exits with code 100 if close fails.
- */
-void close_fds(int fd_from, int fd_to)
-{
 	if (close(fd_from) == -1)
-		print_error_and_exit(100,
-			"Error: Can't close fd %d\n", (void *)(long)fd_from);
-
+		print_error(100, "Error: Can't close fd ", file_from);
 	if (close(fd_to) == -1)
-		print_error_and_exit(100,
-			"Error: Can't close fd %d\n", (void *)(long)fd_to);
+		print_error(100, "Error: Can't close fd ", file_to);
 }
 
 /**
- * main - entry point, checks arguments and performs file copy
- * @argc: argument count
- * @argv: argument vector
+ * main - Entry point
+ * @argc: Argument count
+ * @argv: Argument vector
  *
- * Expects exactly 2 arguments: source and destination file paths.
- * Exits on invalid arguments or any file operation error.
- *
- * Return: 0 on success.
+ * Return: 0 on success, exits with code on failure
  */
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to;
-
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		print_error(97, "Usage: cp file_from file_to", NULL);
 
-	if (strcmp(argv[1], argv[2]) == 0)
-	{
-		dprintf(STDERR_FILENO, "Error: source and destination files are the same\n");
-		exit(99);
-	}
-
-	open_files(argv[1], argv[2], &fd_from, &fd_to);
-	copy_data(fd_from, fd_to, argv[2]);
-	close_fds(fd_from, fd_to);
+	copy_file(argv[1], argv[2]);
 
 	return (0);
 }
